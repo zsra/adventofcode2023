f50ac325110817a6563d956d399b1b5b6aee1717
richard <richard.zsupos@gmail.com>
2023-12-01 18:23:25 +0100

    day1

---
+++ day1/day1.rs
@@ -0,0 +1,26 @@
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+    path::Path,
+};
+
+fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
+    BufReader::new(File::open(filename)?).lines().collect()
+}
+
+fn main() {
+    let lines = lines_from_file("input.in").expect("Could not load lines");
+    let mut sum = 0;
+
+    for line in lines {
+        let left_number = line.chars().find(|c| c.is_digit(10))
+            .map(|c| c.to_digit(10).unwrap() as i32).unwrap_or(-1);
+        
+        let right_number = line.chars().rev().find(|c| c.is_digit(10))
+            .map(|c| c.to_digit(10).unwrap() as i32).unwrap_or(-1);
+
+        sum += left_number * 10 + right_number;
+    }
+
+    println!("{}", sum);
+}
